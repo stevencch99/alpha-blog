f16e1c14b7bf52864e557828ef7d6fa7b741ffe1
Nine_Bro <shinjisrg@gmail.com>
2018-09-13 22:12:02 +0800

    Change pages/home to root page(localhost:3000/)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "pages/home", to: "pages#home"
-  get "pages/about", to: "pages#about"
+  # get "pages/home", to: "pages#home"
+  root "pages#home" # set root page to pages#home
+  # get "pages/about", to: "pages#about"
+  get "about", to: "pages#about"
 end
