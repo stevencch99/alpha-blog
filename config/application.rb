de1ff0644df8d2d59da017eda1bbe869408aac6c
Nine_Bro <shinjisrg@gmail.com>
2018-09-16 15:20:32 +0800

    modify config/application.rb try to fix precompiling assets failed problem.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # 180916 部署至 heroku 推送失敗，故按照 stackoverflow 查到的方法新增這條：
+    # https://stackoverflow.com/questions/19650621/heroku-upload-precompiling-assets-failed
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
